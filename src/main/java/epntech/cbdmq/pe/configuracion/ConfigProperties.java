091033939451b613701955584098f1864ba43fa4
Diego Moreno Rosero <dmoreno@tech.epn.edu.ec>
2023-03-13 21:47:55 -0500

    Sincronización #3 2023-03-13 master

---
+++ src/main/java/epntech/cbdmq/pe/configuracion/ConfigProperties.java
@@ -0,0 +1,25 @@
+package epntech.cbdmq.pe.configuracion;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Configuration
+//@ConfigurationProperties
+@Data
+public class ConfigProperties {
+
+/*	// ARCHIVOS
+	@Value("${spring.servlet.multipart.max-file-size}")
+	public String TAMAÑO_MÁXIMO;
+
+	// EMAIL
+	@Value("${pecb.email.username}")
+	public String USERNAME;
+
+	@Value("${pecb.email.password}")
+	public String PASSWORD;
+*/
+}
