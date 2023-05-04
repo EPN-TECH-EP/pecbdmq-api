ec583845db8258cb3cc0838fb89f8855d7efc566
Pvalverde1996 <pvalverde@tech.epn.edu.ec>
2023-05-04 09:48:40 -0500

    Listado de Inscripciones validas
    Se implemento un exel y un pdf para la impresion

--- src/main/java/epntech/cbdmq/pe/dominio/util/InscripcionesValidasUtil.java
+++ src/main/java/epntech/cbdmq/pe/dominio/util/InscripcionesValidasUtil.java
@@ -6,6 +6,7 @@ import lombok.Data;
 
 @Data
 @Entity
+
 public class InscripcionesValidasUtil {
 
 	@Id
---
+++ src/main/java/epntech/cbdmq/pe/helper/ExelhelperInscripcionvalida.java
@@ -0,0 +1,116 @@
+package epntech.cbdmq.pe.helper;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.web.multipart.MultipartFile;
+
+import epntech.cbdmq.pe.dominio.util.Excel;
+import epntech.cbdmq.pe.dominio.util.InscripcionesValidasUtil;
+
+public class ExelhelperInscripcionvalida {
+	
+	  public static String TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+	  static String[] HEADERs = { "Id_POstulante" };
+	  static String SHEET = "Hoja1";
+	  
+	  public static boolean hasExcelFormat(MultipartFile file) {
+
+		    if (!TYPE.equals(file.getContentType())) {
+		      return false;
+		    }
+
+		    return true;
+		  }
+	  public static ByteArrayInputStream datosToExcel(List<InscripcionesValidasUtil> datos) {
+
+		    try (Workbook workbook = new XSSFWorkbook(); ByteArrayOutputStream out = new ByteArrayOutputStream();) {
+		      Sheet sheet = workbook.createSheet(SHEET);
+
+		      // Header
+		      Row headerRow = sheet.createRow(0);
+
+		      for (int col = 0; col < HEADERs.length; col++) {
+		        Cell cell = headerRow.createCell(col);
+		        cell.setCellValue(HEADERs[col]);
+		      }
+
+		      int rowIdx = 1;
+		      for (InscripcionesValidasUtil dato : datos) {
+		        Row row = sheet.createRow(rowIdx++);
+
+		        row.createCell(0).setCellValue(dato.getId_postulante());
+		        
+		      }
+
+		      workbook.write(out);
+		      return new ByteArrayInputStream(out.toByteArray());
+		    } catch (IOException e) {
+		      throw new RuntimeException("fail to import data to Excel file: " + e.getMessage());
+		    }
+		  }
+	  
+	  public static List<InscripcionesValidasUtil> excelToDatos(InputStream is) {
+		    try {
+		      Workbook workbook = new XSSFWorkbook(is);
+
+		      Sheet sheet = workbook.getSheet(SHEET);
+		      Iterator<Row> rows = sheet.iterator();
+
+		      List<InscripcionesValidasUtil> datos = new ArrayList<InscripcionesValidasUtil>();
+
+		      int rowNumber = 0;
+		      while (rows.hasNext()) {
+		        Row currentRow = rows.next();
+
+		        // skip header
+		        if (rowNumber == 0) {
+		          rowNumber++;
+		          continue;
+		        }
+
+		        Iterator<Cell> cellsInRow = currentRow.iterator();
+
+		        InscripcionesValidasUtil dato = new InscripcionesValidasUtil();
+
+		        int cellIdx = 0;
+		        while (cellsInRow.hasNext()) {
+		          Cell currentCell = cellsInRow.next();
+		          
+		          switch (cellIdx) {
+		          case 0:
+		            //dato.setId((int) currentCell.getNumericCellValue());
+		            break;
+
+		          case 1:
+		            dato.setId_postulante(currentCell.getStringCellValue());
+		            break;
+
+		          default:
+		            break;
+		          }
+
+		          cellIdx++;
+		        }
+
+		        datos.add(dato);
+		      }
+
+		      workbook.close();
+
+		      return datos;
+		    } catch (IOException e) {
+		      throw new RuntimeException("fail to parse Excel file: " + e.getMessage());
+		    }
+		  }
+}
