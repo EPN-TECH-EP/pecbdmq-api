a3b49b2196eea4857115360e92ad82914afc0fb1
Pvalverde1996 <pvalverde@tech.epn.edu.ec>
2023-05-04 15:07:46 -0500

    Crear Actualizar En Rol y Menu
    Implementacion de validaciones

--- src/main/java/epntech/cbdmq/pe/repositorio/admin/RolRepository.java
+++ src/main/java/epntech/cbdmq/pe/repositorio/admin/RolRepository.java
@@ -1,11 +1,16 @@
 package epntech.cbdmq.pe.repositorio.admin;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import epntech.cbdmq.pe.dominio.admin.Aula;
 import epntech.cbdmq.pe.dominio.admin.Rol;
 
 public interface RolRepository extends JpaRepository<Rol, Long>{
 	
 	Rol findRolByCodRol(Integer codRol);
+	
+	Optional<Rol> findByNombreIgnoreCase(String Nombre);
 
 }
--- src/main/java/epntech/cbdmq/pe/servicio/admin/MenuService.java
+++ src/main/java/epntech/cbdmq/pe/servicio/admin/MenuService.java
@@ -1,6 +1,10 @@
 package epntech.cbdmq.pe.servicio.admin;
 
+import static epntech.cbdmq.pe.constante.MensajesConst.REGISTRO_VACIO;
+import static epntech.cbdmq.pe.constante.MensajesConst.REGISTRO_YA_EXISTE;
+
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,8 +13,11 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import epntech.cbdmq.pe.dominio.admin.Aula;
 import epntech.cbdmq.pe.dominio.admin.Menu;
 import epntech.cbdmq.pe.dominio.admin.MenuPermisos;
+import epntech.cbdmq.pe.dominio.admin.MenuRol;
+import epntech.cbdmq.pe.excepcion.dominio.DataException;
 import epntech.cbdmq.pe.repositorio.admin.MenuRepository;
 
 @Service
@@ -32,5 +39,35 @@ public class MenuService {
 	public List<Menu> getAll(){
 		return this.menuRepository.findAll();
 	}
+	
+	public Optional<Menu> getById(Integer id) {
+		// TODO Auto-generated method stub
+		return menuRepository.findById(id);
+	}
+	
+public Menu save(Menu menu) throws DataException {
+	if (menu.getEtiqueta().trim().isEmpty())
+		throw new DataException(REGISTRO_VACIO);
+	Menu objGuardado = menuRepository.findMenuByEtiqueta(menu.getEtiqueta());
+	if (objGuardado !=null) {
+		throw new DataException(REGISTRO_YA_EXISTE);
+	}
+
+	menu.setEtiqueta(menu.getEtiqueta().toUpperCase());
+	return menuRepository.save(menu);
+		
+	}
+	
+	public Menu update(Menu menu) throws DataException {
+		if(menu.getEtiqueta() !=null) {
+			Menu objGuardado = menuRepository.findMenuByEtiqueta(menu.getEtiqueta());
+			if (objGuardado!=null&& !objGuardado.getCodMenu().equals(menu.getCodMenu())) {
+				throw new DataException(REGISTRO_YA_EXISTE);
+			}
+		}
+		return this.save(menu);
+	}
+	
+	
 
 }
