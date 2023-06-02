c6b36398573e7978f1635fffaa95a670b9ddcdfe
JG <ludwing.gomez@epn.edu.ec>
2023-06-02 11:57:13 -0500

    feat: implementacion swagger

--- src/main/java/epntech/cbdmq/pe/configuracion/SwaggerConfig.java
+++ src/main/java/epntech/cbdmq/pe/configuracion/SwaggerConfig.java
@@ -1,36 +1,28 @@
 package epntech.cbdmq.pe.configuracion;
 
+
 import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-
-
-import springfox.documentation.builders.ParameterBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-
-import java.util.Arrays;
-
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI usersMicroserviceOpenAPI() {
+    public OpenAPI customizeOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
-                .info(new Info().title("API Plataforma Educativa Bomberos")
-                        .description("Plataforma educativa")
-                        .version("1.0"));
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
-
 }
 
 
--- src/main/java/epntech/cbdmq/pe/resource/MateriaResource.java
+++ src/main/java/epntech/cbdmq/pe/resource/MateriaResource.java
@@ -4,6 +4,15 @@ import static epntech.cbdmq.pe.constante.MensajesConst.REGISTRO_ELIMINADO_EXITO;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,63 +33,71 @@ import epntech.cbdmq.pe.servicio.impl.MateriaServiceImpl;
 
 @RestController
 @RequestMapping("/materia")
+@Tag(name = "MATERIAAA", description = "Devulve todas las materias con sus respectivos detalles")
 //@CrossOrigin(origins = "${cors.urls}")
 public class MateriaResource {
-	
-	@Autowired
-	private MateriaServiceImpl objService;
 
-	@PostMapping("/crear")
-	@ResponseStatus(HttpStatus.CREATED)
-	public ResponseEntity<?> guardar(@RequestBody Materia obj) throws DataException{
-		obj.setNombre(obj.getNombre().toUpperCase());
-		return new ResponseEntity<>(objService.save(obj), HttpStatus.OK);
-	}
-	
-	@GetMapping("/listar")
-	public List<Materia> listar() {
-		return objService.getAll();
-	}
+    @Autowired
+    private MateriaServiceImpl objService;
 
-	@GetMapping("/{id}")
-	public ResponseEntity<Materia> obtenerPorId(@PathVariable("id") int codigo) {
-		return objService.getById(codigo).map(ResponseEntity::ok)
-				.orElseGet(() -> ResponseEntity.notFound().build());
-	}
+    @Operation(summary = "Crea una materia a partir de un objeto materia")
+    @PostMapping("/crear")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> guardar(@RequestBody Materia obj) throws DataException {
+        obj.setNombre(obj.getNombre().toUpperCase());
+        return new ResponseEntity<>(objService.save(obj), HttpStatus.OK);
+    }
+    @Operation(summary = "Trae la lista de materias")
+    @GetMapping("/listar")
+    public List<Materia> listar() {
+        return objService.getAll();
+    }
 
-	@PutMapping("/{id}")
-	public ResponseEntity<Materia> actualizarDatos(@PathVariable("id") int codigo, @RequestBody Materia obj) throws DataException{
-		return (ResponseEntity<Materia>) objService.getById(codigo).map(datosGuardados -> {
-			datosGuardados.setNombre(obj.getNombre().toUpperCase());
-			//datosGuardados.setNumHoras(obj.getNumHoras());
-			datosGuardados.setTipoMateria(obj.getTipoMateria());
-			//datosGuardados.setObservacionMateria(obj.getObservacionMateria());
-			//datosGuardados.setPesoMateria(obj.getPesoMateria());
-			//datosGuardados.setNotaMinima(obj.getNotaMinima());
-			datosGuardados.setEstado(obj.getEstado());
+    @Operation(summary = "Trae una materia por ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = Materia.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", description = "The Tutorial with given Id was not found.", content = {@Content(schema = @Schema())})
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<Materia> obtenerPorId(@PathVariable("id") int codigo) {
+        return objService.getById(codigo).map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
-			Materia datosActualizados = null;
-			
-			try {
-				datosActualizados = objService.update(datosGuardados);
-				} catch (DataException e) {
-					// TODO Auto-generated catch block
-					//e.printStackTrace();
-					return response(HttpStatus.BAD_REQUEST, e.getMessage().toString());
-				}
-				return new ResponseEntity<>(datosActualizados, HttpStatus.OK);
-			
-			
-		}).orElseGet(() -> ResponseEntity.notFound().build());
-	}
-	
-	@DeleteMapping("/{id}")
-	public ResponseEntity<HttpResponse> eliminarDatos(@PathVariable("id") int codigo) throws DataException {
-		objService.delete(codigo);
-		return response(HttpStatus.OK, REGISTRO_ELIMINADO_EXITO);
-	}
-	
-	private ResponseEntity<HttpResponse> response(HttpStatus httpStatus, String message) {
+    @PutMapping("/{id}")
+    public ResponseEntity<Materia> actualizarDatos(@Parameter(description = "Objeto de tipo materia, en donde se encuentran las propiedades ya actualizadas", required = true) @PathVariable("id") int codigo,
+                                                   @RequestBody Materia obj) throws DataException {
+        return (ResponseEntity<Materia>) objService.getById(codigo).map(datosGuardados -> {
+            datosGuardados.setNombre(obj.getNombre().toUpperCase());
+            //datosGuardados.setNumHoras(obj.getNumHoras());
+            datosGuardados.setTipoMateria(obj.getTipoMateria());
+            //datosGuardados.setObservacionMateria(obj.getObservacionMateria());
+            //datosGuardados.setPesoMateria(obj.getPesoMateria());
+            //datosGuardados.setNotaMinima(obj.getNotaMinima());
+            datosGuardados.setEstado(obj.getEstado());
+
+            Materia datosActualizados = null;
+
+            try {
+                datosActualizados = objService.update(datosGuardados);
+            } catch (DataException e) {
+                // TODO Auto-generated catch block
+                //e.printStackTrace();
+                return response(HttpStatus.BAD_REQUEST, e.getMessage().toString());
+            }
+            return new ResponseEntity<>(datosActualizados, HttpStatus.OK);
+
+
+        }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpResponse> eliminarDatos(@Parameter(description = "Buscar materia con ese id", required = true) @PathVariable("id") int codigo) throws DataException {
+        objService.delete(codigo);
+        return response(HttpStatus.OK, REGISTRO_ELIMINADO_EXITO);
+    }
+
+    private ResponseEntity<HttpResponse> response(HttpStatus httpStatus, String message) {
         return new ResponseEntity<>(new HttpResponse(httpStatus.value(), httpStatus, httpStatus.getReasonPhrase().toUpperCase(),
                 message), httpStatus);
     }
