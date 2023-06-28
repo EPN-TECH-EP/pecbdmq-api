5f7f830df9fd20c1d097b842eb553932b0faf57c
JG <ludwing.gomez@epn.edu.ec>
2023-06-28 09:45:37 -0500

    bug: Se intento cambiar nombres pero sigue sin funcionar
    Error dinamic mach

--- src/main/java/epntech/cbdmq/pe/dominio/admin/formacion/InstructorMateriaParalelo.java
+++ src/main/java/epntech/cbdmq/pe/dominio/admin/formacion/InstructorMateriaParalelo.java
@@ -1,5 +1,6 @@
 package epntech.cbdmq.pe.dominio.admin.formacion;
 
+import epntech.cbdmq.pe.dominio.fichaPersonal.profesionalizacion.ProfesionalizacionEstudiante;
 import epntech.cbdmq.pe.dominio.util.UsuarioDtoRead;
 import jakarta.persistence.*;
 import lombok.Data;
@@ -7,7 +8,7 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name ="gen_instructor_materia_paralelo")
-@NamedNativeQuery(name = "InformacionMateriaDto.getInformacionMateria",
+@NamedNativeQuery(name = "InformacionMateriaDto.find",
         query = "select gmp.cod_materia_paralelo , gm.nombre_materia,em.nombre_eje_materia,p.nombre_paralelo,a.cod_aula from cbdmq.gen_instructor_materia_paralelo gimp\n" +
                 "left join cbdmq.gen_materia_paralelo gmp on gmp.cod_materia_paralelo = gimp.cod_materia_paralelo \n" +
                 "left join cbdmq.gen_materia_periodo gmpe on gmpe.cod_materia_periodo = gmp.cod_materia_periodo \n" +
@@ -15,9 +16,11 @@ import lombok.Data;
                 "left join cbdmq.gen_eje_materia em on em.cod_eje_materia = gm.cod_eje_materia \n" +
                 "left join cbdmq.gen_paralelo p on gmp.cod_paralelo = p.cod_paralelo \n" +
                 "left join cbdmq.gen_aula a on gmpe.cod_aula = a.cod_aula \n" +
-                "group by (gmp.cod_materia_paralelo,gm.nombre_materia,em.nombre_eje_materia,p.nombre_paralelo, a.cod_aula)\n",
+                "group by (gmp.cod_materia_paralelo,gm.nombre_materia,em.nombre_eje_materia,p.nombre_paralelo, a.cod_aula)"
+        ,
         resultSetMapping = "InformacionMateriaDto"
 )
+
 @SqlResultSetMapping(name = "InformacionMateriaDto", classes = @ConstructorResult(targetClass = InformacionMateriaDto.class, columns = {
         @ColumnResult(name = "cod_materia_paralelo"),
         @ColumnResult(name = "nombre_materia"),
