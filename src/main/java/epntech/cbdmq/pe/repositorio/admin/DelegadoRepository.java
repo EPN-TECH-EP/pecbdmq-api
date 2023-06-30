74e230c528c6b6e79400cf2ee3f66a34aa2c6d90
JG <ludwing.gomez@epn.edu.ec>
2023-06-30 15:45:27 -0500

    feat: Arreglo query isDelegado en PA

--- src/main/java/epntech/cbdmq/pe/repositorio/admin/DelegadoRepository.java
+++ src/main/java/epntech/cbdmq/pe/repositorio/admin/DelegadoRepository.java
@@ -20,6 +20,6 @@ public interface DelegadoRepository extends JpaRepository<Delegado, DelegadoPK> 
 	@Query(value="select gd.* from cbdmq.gen_delegado gd \n" +
 			"left join cbdmq.gen_usuario gu on gd.cod_usuario = gu.cod_usuario \n" +
 			"where gu.cod_usuario = :codUsuario\n" +
-			"and gu.cod_periodo_academico =: codPA", nativeQuery = true)
+			"and gd.cod_periodo_academico =:codPA", nativeQuery = true)
 	Optional<Delegado> delegadoByUser(@Param("codUsuario") Integer codUsuario,@Param("codPA") Integer codPA);
 }
