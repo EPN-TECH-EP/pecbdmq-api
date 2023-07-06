b411a2f1e12acb0543c3e6e76a3963816407c274
JG <ludwing.gomez@epn.edu.ec>
2023-07-06 08:14:47 -0500

    feat: esquema para asignar estudiantes

--- src/main/java/epntech/cbdmq/pe/servicio/formacion/EstudianteMateriaParaleloService.java
+++ src/main/java/epntech/cbdmq/pe/servicio/formacion/EstudianteMateriaParaleloService.java
@@ -2,10 +2,13 @@ package epntech.cbdmq.pe.servicio.formacion;
 
 import epntech.cbdmq.pe.dominio.admin.formacion.EstudianteMateriaParalelo;
 import epntech.cbdmq.pe.dominio.admin.formacion.InstructorMateriaParalelo;
+import epntech.cbdmq.pe.dominio.fichaPersonal.Estudiante;
 
 import java.util.List;
 
 public interface EstudianteMateriaParaleloService {
     public List<EstudianteMateriaParalelo> getEstudiantesMateriaParalelo();
     public EstudianteMateriaParalelo save(EstudianteMateriaParalelo newObj);
+    public Boolean asignarEstudianteMateriaParalelo(Integer codMateria, Integer codParalelo, Estudiante[] estudiantes);
+
 }
--- src/main/java/epntech/cbdmq/pe/servicio/impl/formacion/EstudianteMateriaParaleloServiceImpl.java
+++ src/main/java/epntech/cbdmq/pe/servicio/impl/formacion/EstudianteMateriaParaleloServiceImpl.java
@@ -1,6 +1,7 @@
 package epntech.cbdmq.pe.servicio.impl.formacion;
 
 import epntech.cbdmq.pe.dominio.admin.formacion.EstudianteMateriaParalelo;
+import epntech.cbdmq.pe.dominio.fichaPersonal.Estudiante;
 import epntech.cbdmq.pe.repositorio.admin.formacion.EstudianteMateriaParaleloRepository;
 import epntech.cbdmq.pe.servicio.formacion.EstudianteMateriaParaleloService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,11 @@ public class EstudianteMateriaParaleloServiceImpl implements EstudianteMateriaPa
     @Override
     public EstudianteMateriaParalelo save(EstudianteMateriaParalelo newObj) {
         return repoObj.save(newObj);
+    }
+
+    @Override
+    public Boolean asignarEstudianteMateriaParalelo(Integer codMateria, Integer codParalelo, Estudiante[] estudiantes) {
+        return null;
     }
 
 }
