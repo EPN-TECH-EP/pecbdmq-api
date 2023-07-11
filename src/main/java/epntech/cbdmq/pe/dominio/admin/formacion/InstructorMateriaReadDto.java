e40f67898997e6480f03a7c315de2b72db9bc1f1
JG <ludwing.gomez@epn.edu.ec>
2023-07-11 16:01:31 -0500

    fix:estandarizacion

--- src/main/java/epntech/cbdmq/pe/dominio/admin/formacion/InstructorMateriaReadDto.java
+++ src/main/java/epntech/cbdmq/pe/dominio/admin/formacion/InstructorMateriaReadDto.java
@@ -11,15 +11,15 @@ public class InstructorMateriaReadDto {
     private Instructor[] instructores;
     private Instructor[] asistentes;
     private Instructor coordinador;
-    private Integer cod_aula;
-    private String nombre_aula;
+    private Integer codAula;
+    private String nombreAula;
 
-    public InstructorMateriaReadDto(Integer codMateria, String nombre, String nombreEje, Integer cod_aula, String nombre_aula) {
+    public InstructorMateriaReadDto(Integer codMateria, String nombre, String nombreEje, Integer codAula, String nombreAula) {
         this.codMateria = codMateria;
         this.nombre = nombre;
         this.nombreEje = nombreEje;
-        this.cod_aula = cod_aula;
-        this.nombre_aula = nombre_aula;
+        this.codAula = codAula;
+        this.nombreAula = nombreAula;
         this.asistentes = new Instructor[0];
         this.instructores = new Instructor[0];
     }
