c39cd1045c6e884b6b8e9e09b50e86c3f1dccb70
JG <ludwing.gomez@epn.edu.ec>
2023-08-03 16:34:46 -0500

    feat: correccion api ciudadano

--- src/main/java/epntech/cbdmq/pe/dominio/util/ApiBaseCiudadano.java
+++ src/main/java/epntech/cbdmq/pe/dominio/util/ApiBaseCiudadano.java
@@ -2,11 +2,13 @@ package epntech.cbdmq.pe.dominio.util;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class ApiBaseCiudadano {
 
 
     private String status;
-    private CiudadanoApiDto data;
+    private List<CiudadanoApiDto> data;
     private String message;
 }
