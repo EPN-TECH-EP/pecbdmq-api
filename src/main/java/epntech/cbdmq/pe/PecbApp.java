c35990e79ac8b7d68b47d916df0cdb16e7412ced
JG <ludwing.gomez@epn.edu.ec>
2023-08-04 11:14:21 -0500

    feat: seguridadPatch

--- src/main/java/epntech/cbdmq/pe/PecbApp.java
+++ src/main/java/epntech/cbdmq/pe/PecbApp.java
@@ -34,7 +34,7 @@ public class PecbApp {
 				"Access-Control-Request-Method", "Access-Control-Request-Headers", "X-API-Key"));
 		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
 				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS"));
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
 		return new CorsFilter(urlBasedCorsConfigurationSource);
 	}
