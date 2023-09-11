582894100e299a42ae91e3964d5ab3b0538301e4
JG <ludwing.gomez@epn.edu.ec>
2023-09-11 11:58:43 -0500

    fix: Enviar la notificacion por correo a cada aspirante indicando info

--- src/main/java/epntech/cbdmq/pe/servicio/PruebaDetalleService.java
+++ src/main/java/epntech/cbdmq/pe/servicio/PruebaDetalleService.java
@@ -31,6 +31,7 @@ public interface PruebaDetalleService {
     public String getTipoResultado(int codSubtipoPrueba);
 
 	public Boolean reordenar(List<PruebaDetalleOrden> listaOrden) throws DataException;
+	void notificarPruebaDetalle(Long codPruebaDetalle) throws DataException;
 
 
 }
