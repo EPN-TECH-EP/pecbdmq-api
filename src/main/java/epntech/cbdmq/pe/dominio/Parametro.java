09c3ed764e3ae7744f7882ad50d95c7cd012cec7
Pedro <fernandoaponte1103@gmail.com>
2023-10-03 20:27:02 -0500

    Feature: Se arreglan endpoints para administración de parámetros

--- src/main/java/epntech/cbdmq/pe/dominio/Parametro.java
+++ src/main/java/epntech/cbdmq/pe/dominio/Parametro.java
@@ -1,22 +1,28 @@
 package epntech.cbdmq.pe.dominio;
 
 import epntech.cbdmq.pe.dominio.profesionalizacion.ProfesionalizacionEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.ResultCheckStyle;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Data
 @Entity
 @Table(name = "gen_parametro")
+@SQLDelete(sql = "UPDATE {h-schema}gen_parametro SET estado = 'ELIMINADO' WHERE cod_parametro = ?", check = ResultCheckStyle.COUNT)
+@Where(clause = "estado <> 'ELIMINADO'")
 public class Parametro extends ProfesionalizacionEntity {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include()
 	@Column(name = "cod_parametro")
 	private Long codParametro;
 	@Column(name = "nombre_parametro")
 	private String nombreParametro;
 	@Column(name = "valor")
 	private String valor;
+
 }
--- src/main/java/epntech/cbdmq/pe/resource/ParametroResource.java
+++ src/main/java/epntech/cbdmq/pe/resource/ParametroResource.java
@@ -1,16 +1,27 @@
 package epntech.cbdmq.pe.resource;
 
 import epntech.cbdmq.pe.dominio.Parametro;
+import epntech.cbdmq.pe.excepcion.dominio.DataException;
 import epntech.cbdmq.pe.repositorio.ParametroRepository;
 import epntech.cbdmq.pe.resource.profesionalizacion.ProfesionalizacionResource;
 import epntech.cbdmq.pe.servicio.impl.profesionalizacion.ParametroServiceImpl;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/parametro")
 public class ParametroResource extends ProfesionalizacionResource<Parametro, Long, ParametroRepository, ParametroServiceImpl> {
     public ParametroResource(ParametroServiceImpl service) {
         super(service);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> actualizarDatos(@PathVariable("id") Long codigo, @RequestBody Parametro obj) throws DataException {
+        return service.findById(codigo).map(datosGuardados-> {
+            datosGuardados.setNombreParametro(obj.getNombreParametro());
+            datosGuardados.setValor(obj.getValor());
+            datosGuardados.setEstado(obj.getEstado());
+            return super.actualizarDatos(datosGuardados);
+        }).orElseGet(()->ResponseEntity.notFound().build());
     }
 }
